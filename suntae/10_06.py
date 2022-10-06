01e289ceef1031ce981adc5eba68c5dbd593c927
HSUNEH <100556640+HSUNEH@users.noreply.github.com>
2022-10-06 16:12:14 +0900

    Create 10_06.py

---
+++ suntae/10_06.py
@@ -0,0 +1,15 @@
+from itertools import permutations
+
+def solution(k, dungeons):
+    result =0 
+    for i in permutations(dungeons, len(dungeons)):
+        k_ = k
+        n = 0
+        for d in i:
+            if k_>= d[0]:
+                k_ -= d[1]
+                n +=1
+            else:
+                result = max(result, n)
+
+    return result
